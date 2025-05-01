00256a650ab69c78aaa137874e3370ae4c33b1b0
Francisco <github.backstage538@passmail.net>
2025-05-01 17:38:44 +0200

    Animate body parts

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className='flex min-h-screen flex-col items-center bg-blue-200 gap-8 p-8'>
       <h1 className='text-4xl font-bold text-pink'>Hangman</h1>
-      <HangmanFigure />
+      <HangmanFigure remainingGuesses={4}/>
     </div>
   );
 }
