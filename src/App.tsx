e1f870b6defc2c9c0458cc51922981e544032372
Francisco <github.backstage538@passmail.net>
2025-05-01 19:57:41 +0200

    Show letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import HangmanFigure from './components/HangmanFigure';
+import WordDisplay from './components/WordDisplay';
 
 export default function App() {
   return (
-    <div className='flex min-h-screen flex-col items-center bg-blue-200 gap-8 p-8'>
+    <div className='flex min-h-screen flex-col items-center gap-8 bg-blue-200 p-8'>
       <h1 className='text-4xl font-bold text-pink'>Hangman</h1>
-      <HangmanFigure remainingGuesses={4}/>
+      <HangmanFigure remainingGuesses={4} />
+      <div className='text-center text-gray-100'>
+        <p className='mb-2 text-xl sm:text-2xl'>
+          Remaining guesses: <span className='font-bold'>6</span>
+        </p>
+        <p className='text-lg'>Tip: something to say</p>
+      </div>
+      <WordDisplay word='vercel' guessedLetters={new Set(['v', 'c', 'e'])} />
     </div>
   );
 }
---
+++ src/components/WordDisplay.tsx
@@ -0,0 +1,22 @@
+interface WordDisplayProps {
+  word: string;
+  guessedLetters: Set<string>;
+}
+
+export default function WordDisplay({
+  word,
+  guessedLetters,
+}: WordDisplayProps) {
+  return (
+    <div className='flex flex-wrap space-x-1 sm:space-x-2'>
+      {word.split('').map((letter, index) => (
+        <div
+          key={index}
+          className='flex h-10 w-6 justify-center border-b-2 border-gray-100 pb-2 text-2xl font-bold uppercase text-white sm:h-12 sm:w-8'
+        >
+          {guessedLetters.has(letter) ? letter : ''}
+        </div>
+      ))}
+    </div>
+  );
+}
